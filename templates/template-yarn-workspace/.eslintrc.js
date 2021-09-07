6de36c771da4c5d961566e6c2d96c4db71c202e2
hanrenguang <413526182@qq.com>
2021-09-07 11:58:11 +0800

    feat(template): 添加模板开发环境相应配置

---
+++ templates/template-yarn-workspace/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:node/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 12
+  }
+}
